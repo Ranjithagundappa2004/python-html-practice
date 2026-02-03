aa8bdfc4a1fac462ea6113db863fc0ce882dca5f
Ranjitha Gundappa <ranjithagr2004@gmail.com>
2026-02-03 19:57:27 +0530

    Day 01: Python practice

---
+++ basic.py
@@ -0,0 +1,25 @@
+class fan:
+    def __init__(self):
+        self.brand="Usha"
+        self.color="white"
+        self.cost=1500
+        self.wings=3
+
+    def on(self):
+        print("fan is on")
+
+    def rotate(self):
+        print("fan is rotating")
+
+    def off(self):
+        print("fan is off")
+
+f1=fan()
+print(f1.brand)
+print(f1.color)
+print(f1.cost)
+print(f1.wings)
+
+f1.on()
+f1.rotate()
+f1.off()
